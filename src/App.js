c33eea107c0681e79f112bbd42a7d2dc34256962
Francis Aerol Alegre <francisaerol@francis-macbook.local>
2021-11-06 10:35:48 -0400

    Add calendar, sidebar and home

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+
+import SidebarMenu from './components/SidebarMenu';
+import RaceRouter from './components/RaceRouter';
+import {
+  BrowserRouter as Router
+} from "react-router-dom";
+
+import 'primereact/resources/themes/vela-blue/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    return (<div className="App">
+      <Router >
+      <SidebarMenu />
+      <RaceRouter />
+      </Router>
+    </div>);
+  };
 }
 
 export default App;
